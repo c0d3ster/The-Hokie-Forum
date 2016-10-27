1a80caf5b5ca92238dbdc709bc085836f12ee6f2
Nick <ntrettel@vt.edu>
2016-10-27 14:23:10 -0400

    Fixed config.php

--- app/config.php
+++ app/config.php
@@ -2,13 +2,13 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost');
-define('IMAGES', 'http://localhost/public/img');
-define('SCRIPTS', 'http://localhost/public/js');
-define('STYLES', 'http://localhost/public/css');
+define('BASE_URL','http://thehokieforum.com');
+define('IMAGES', 'http://thehokieforum.com/public/img');
+define('SCRIPTS', 'http://thehokieforum.com/public/js');
+define('STYLES', 'http://thehokieforum.com/public/css');
 
 // database constants
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASS','');
+define('DB_PASS','ooeyGUIs');
 define('DB_DATABASE','hokieforum');
