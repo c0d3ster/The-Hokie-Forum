f789b8851878f09ac3fb12fb0d5d8330c398ca38
Nick <ntrettel@vt.edu>
2016-11-13 22:48:36 -0500

    favorites

--- app/model/Favorite.class.php
+++ app/model/Favorite.class.php
@@ -82,7 +82,7 @@ class Favorite extends DbObject {
 		$fav = self::getFavoritesByTopicId($favorite->get('topic_id'));
 		foreach($fav as $f) {
 			if($f['user_id'] == $favorite->get('user_id'))
-				return $f;
+				return $favorite;
 		}
 		return null;
 	}
